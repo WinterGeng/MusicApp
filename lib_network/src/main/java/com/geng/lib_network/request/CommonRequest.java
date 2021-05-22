d10324929653130b0db8c2865d3af1546cce2904
gengbingdong <1558270516@qq.com>
2021-05-22 16:24:32 +0800

    文件上传请求构造

--- lib_network/src/main/java/com/geng/lib_network/request/CommonRequest.java
+++ lib_network/src/main/java/com/geng/lib_network/request/CommonRequest.java
@@ -1,10 +1,14 @@
 package com.geng.lib_network.request;
 
+import java.io.File;
 import java.util.Map;
 
 import okhttp3.FormBody;
 import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 /**
  * 对外提供post文件上传请求
@@ -63,5 +67,23 @@ public class CommonRequest {
                 headers(mHeaderBuilder.build()).
                 get().
                 build();
+    }
+
+    public static final MediaType FILE_TYPE = MediaType.parse("application/octet-stream");
+
+    //文件上传请求对象
+    public static Request createMultiPostRequest(String url, RequestParams params, RequestParams headers) {
+        MultipartBody.Builder requestBody = new MultipartBody.Builder();
+        requestBody.setType(MultipartBody.FORM);
+        if (params != null) {
+            for (Map.Entry<String, Object> entry : params.fileParams.entrySet()) {
+                if (entry.getValue() instanceof File) {
+                    requestBody.addPart(Headers.of("Content-Disposition", "form-data; name=\"" + entry.getKey() + "\""), RequestBody.create(FILE_TYPE, (File) entry.getValue()));
+                } else if (entry.getValue() instanceof String) {
+                    requestBody.addPart(Headers.of("Content-Disposition", "form-data; name=\"" + entry.getKey() + "\""), RequestBody.create(null, (String) entry.getValue()));
+                }
+            }
+        }
+        return new Request.Builder().url(url).post(requestBody.build()).build();
     }
 }
